6b8b2435a253d700de40d79aa0b1e476a5cf8614
lyem <rfael403@gmail.com>
2021-06-20 18:15:29 -0300

    cors

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ app = Flask(__name__)
 cors = CORS(app)
 api = Api(app)
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
 api.add_resource(On, '/')
 api.add_resource(RegisterHospital, '/register/hospital')
 api.add_resource(RegisterUser, '/register/user')
