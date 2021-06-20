6d74b5860983456b6e55e96c8875a5e3033e2772
lyem <rfael403@gmail.com>
2021-06-20 19:15:01 -0300

    cors

--- features/register_hospital/register_hospital.py
+++ features/register_hospital/register_hospital.py
@@ -1,14 +1,12 @@
 from flask import request
 from flask_restful import Resource
-from flask_cors import CORS
 from features.register_hospital.model.hospital_model import HospitalModel
 from shared.db import Db
 
-@cross_origin()
 class RegisterHospital(Resource):
     def post(self):
         data = request.json
         hospital = HospitalModel(data)
         db = Db()
         db.insert('hospital', hospital.toJson())
-        return {'status': 'ok'}
+        return {'status': 'ok'}, {'Access-Control-Allow-Origin': '*'}
--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 from flask import Flask
-from flask_cors import CORS
 from flask_restful import Api
 from features.on.on import On
 from features.register_hospital.register_hospital import RegisterHospital
 from features.register_user.register_user import RegisterUser
 
 app = Flask(__name__)
-cors = CORS(app)
-cors = CORS(app, resources={
-	r"/*":{
-		"origins": "http://127.0.0.1:5500"
-	}
-})
 api = Api(app)
 
 api.add_resource(On, '/')
