97cd9154e7830afa5312f63879274f544b69fe24
lyem <rfael403@gmail.com>
2021-06-20 18:19:10 -0300

    cors

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ api = Api(app)
 @app.after_request
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
     return response
 
