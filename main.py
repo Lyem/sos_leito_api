5bc4b5fef27be8b9ac0863cfc1f6731c06791483
lyem <rfael403@gmail.com>
2021-06-20 17:44:38 -0300

    cors

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Api
 from features.on.on import On
 from features.register_hospital.register_hospital import RegisterHospital
 from features.register_user.register_user import RegisterUser
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 api = Api(app)
 
 api.add_resource(On, '/')
